957c93d7bdb5ae769e5564ed0dd88f7f99b3a84d
dylant <ldduy.edu99@gmail.com>
2021-06-21 18:48:49 +0700

    render html page

--- app/simple_router.py
+++ app/simple_router.py
@@ -1,6 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 simple = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
+
 
 @simple.get('/simple')
 async def simple_page():
     return {"message": "here is simple router"}
+
+@simple.get('/base')
+async def base_page(request: Request):
+
+    # return HTMLResponse(content=content)
+    # return "haha"
+    return templates.TemplateResponse("base.html",{"request": request})
